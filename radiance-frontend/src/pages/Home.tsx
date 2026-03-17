8f515ad176113b88c095b1c1cd84f70e7eefcaa0
Do Heon Kim <cola_hanuel@your.email>
2026-03-17 18:51:14 -0500

    feat: added glass panel section

--- radiance-frontend/src/pages/Home.tsx
+++ radiance-frontend/src/pages/Home.tsx
@@ -3,11 +3,13 @@ import HeroSection from "../components/hero/HeroSection";
 import LogoCarousel from "../components/sections/LogoCarousel";
 import BottomBar from "../components/hero/BottomBar";
 import { heroContent } from "../constants/hero";
+import GlassPanelsSection from "../components/sections/GlassPanelsSection";
 
 export default function Home() {
   return (
     <PageLayout>
       <HeroSection />
+      <GlassPanelsSection />
       <LogoCarousel />
       <BottomBar
         location={heroContent.footer.location}
