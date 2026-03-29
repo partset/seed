200168b12692b02f7506f2c23042ae9cf46c8c28
Alex Pham <phamalex05@gmail.com>
2026-03-29 12:22:10 -0500

    fix: I deleted the extra sql and fixed the route

--- radiance-backend/routes/adminRoutes.js
+++ radiance-backend/routes/adminRoutes.js
@@ -3,10 +3,11 @@ const { checkAdmin } = require("../controllers/admin/checkAdmin");
 const { getCompanies } = require("../controllers/admin/getCompanies");
 
 const { requireSupabaseAuth } = require("../middleware/requireSupabaseAuth");
+const { requireAdmin } = require("../middleware/requireAdmin");
 
 const router = express.Router();
 
 router.post("/check-admin", requireSupabaseAuth, checkAdmin);
-router.get("/companies", requireSupabaseAuth, getCompanies);
+router.get("/companies", requireSupabaseAuth, requireAdmin, getCompanies);
 
 module.exports = router;
