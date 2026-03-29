2cf5d3a35a74b7f874ac5104fff7798d261d4b8b
Alex Pham <phamalex05@gmail.com>
2026-03-29 15:15:09 -0500

    feat: i added the api to the services

---
+++ radiance-frontend/src/services/api/company/getCompanyById/api.ts
@@ -0,0 +1,26 @@
+import type { Company } from "../../../../types/company";
+import type { ApiResponse } from "../../response";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export async function getCompanyById(
+  token: string,
+  companyId: string,
+): Promise<Company> {
+  const response = await fetch(`${API_BASE_URL}/api/company/${companyId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const result: ApiResponse<Company> = await response.json();
+  console.log("API response for getCompanyById:", result);
+
+  if (!response.ok || !result.success) {
+    throw new Error(result.error || "Failed to fetch company");
+  }
+
+  return result.data ?? [];
+}
